6ef222650a9e0ace459060ad70ac45e57e7d9411
perssonne <143746092+perssonne@users.noreply.github.com>
2023-09-14 10:46:55 -0400

    MAJ

--- main.py
+++ main.py
@@ -2,22 +2,26 @@
 
 import random
 
-def boucle_jeu():
-    chiffre_aléatoire = random.randint(0,1000)
-    print("L'ordinateur a choisi un nombre entre 0 et 1000.")
-    while boucle_jeu=False:
-        essai=str(int("Entrez votre essai:"))
-        if essai>chiffre_aléatoire:
-            print("Trop grand.")
-            nb_essais=nb_essais+1
-        elif essai<chiffre_aléatoire:
-            print("Trop petit.")
-            nb_essais=nb_essais+1
-        elif essai==chiffre_aléatoire:
-            print("Vous avez réussi en",nb_essais," essais. Bravo!")
-            boucle_jeu = True
-        else:
-            print("Erreur")
+chiffre_aléatoire = random.randint(0,1000)
+print("L'ordinateur a choisi un nombre entre 0 et 1000.")
+nb_essais=0
+boucle_jeu=True
+while boucle_jeu:
+    essai=int(input("Entrez votre essai:"))
+    if essai > chiffre_aléatoire:
+        print("Trop grand.")
+        nb_essais=nb_essais+1
+        boucle_jeu = True
+    elif essai<chiffre_aléatoire:
+        print("Trop petit.")
+        nb_essais=nb_essais+1
+        boucle_jeu = True
+    elif essai==chiffre_aléatoire:
+        print("Vous avez réussi en",nb_essais,"essais. Bravo!")
+        print()
+        boucle_jeu = False
+    else:
+        print("Erreur")
+        boucle_jeu = True
 
-boucle_jeu()
-
+rejouer=str(input('Voulez vous rejouer oui ou non?'))
