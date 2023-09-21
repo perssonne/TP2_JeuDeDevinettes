4b8199677b1afcdf06153e6e069821d405f5e301
sanchezvaleroe <sanchezvaleroe@educ.college-montreal.qc.ca>
2023-09-21 12:10:06 -0400

    MAJ

--- main.py
+++ main.py
@@ -2,26 +2,41 @@
 
 import random
 
-chiffre_aléatoire = random.randint(0,1000)
-print("L'ordinateur a choisi un nombre entre 0 et 1000.")
-nb_essais=0
-boucle_jeu=True
-while boucle_jeu:
-    essai=int(input("Entrez votre essai:"))
-    if essai > chiffre_aléatoire:
-        print("Trop grand.")
-        nb_essais=nb_essais+1
-        boucle_jeu = True
-    elif essai<chiffre_aléatoire:
-        print("Trop petit.")
-        nb_essais=nb_essais+1
-        boucle_jeu = True
-    elif essai==chiffre_aléatoire:
-        print("Vous avez réussi en",nb_essais,"essais. Bravo!")
-        print()
-        boucle_jeu = False
+def jeu():
+    bornes=str(input('Voulez-vous choisir vous-même l intervalle du nombre à découvrir? (y/n)'))
+    if bornes == ('y'):
+        borne_minimale=int(input('Quelle sera la borne minimale?'))
+        borne_maximale=int(input('Quelle sera la borne maximale?'))
+    elif bornes == ('n'):
+        borne_minimale=0
+        borne_maximale=100
     else:
-        print("Erreur")
-        boucle_jeu = True
+        print('Erreur')
 
-rejouer=str(input('Voulez vous rejouer oui ou non?'))
+    chiffre_aléatoire = random.randint(borne_minimale,borne_maximale)
+    print(f'L ordinateur a choisi un nombre entre {borne_minimale} et {borne_maximale}.')
+    nb_essais=1
+
+    while True:
+
+        essai=int(input("Entrez votre essai:"))
+
+        if essai > chiffre_aléatoire:
+            print("Trop grand.")
+            nb_essais=nb_essais+1
+        elif essai<chiffre_aléatoire:
+            print("Trop petit.")
+            nb_essais=nb_essais+1
+        elif essai==chiffre_aléatoire:
+            print("Vous avez réussi en",nb_essais,"essais. Bravo!")
+            quit=str(input('Voulez-vous refaire une partie? (y/n)'))
+            if quit == ('y'):
+                jeu()
+            elif quit == ('n'):
+                print('Merci et au revoir.')
+                break
+        else:
+            print("Erreur")
+
+print('Bienvenue dans ce jeu de devinette.')
+jeu()
